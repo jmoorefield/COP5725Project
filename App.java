afca6d19e09e22ecdf8be4414d5534f1e68850b8
jmoorefield <74016505+jmoorefield@users.noreply.github.com>
2023-12-04 05:14:49 -0500

    finished dfs tree

--- App.java
+++ App.java
@@ -3,79 +3,164 @@ package IGS;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Random;
+import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-// include Table 1 statistics (out-degree statistics) in report? 
-
+// TODO: change to 24,329 nodes 
 public class App {
-    public static void main(String[] args) throws IOException {
-        String dataPath = "/Users/jess/IGSProject/data/metadata_simplified.json";
 
-        BufferedReader reader = new BufferedReader(new FileReader(dataPath));
-        List<Entry> jsonEntries = new Gson().fromJson(reader, new TypeToken<List<Entry>>() {
-        }.getType());
-        reader.close();
+    public static String getRandomTarget(DFS graph) {
+        List<String> listofNodes = new ArrayList<String>(graph.getNodes());
+        listofNodes.remove("*"); // do not set root to target node
+        Random r = new Random();
+        return listofNodes.get(r.nextInt(listofNodes.size()));
+    }
 
-        // default initialized with "root" as the root
-        DFS myGraph = new DFS(jsonEntries.size());
+    // may be easier to do with sydney's implementation?
+    public static void getOutDegreeStats(Map<Integer, ArrayList<String>> l) {
+        System.out.println("Depth vs. Number of Nodes");
+        System.out.println("----------------------------");
 
-        Iterator<Entry> jsonItr = jsonEntries.iterator();
+        Iterator<Map.Entry<Integer, ArrayList<String>>> itr = l.entrySet().iterator();
+        while (itr.hasNext()) {
+            Map.Entry<Integer, ArrayList<String>> entry = itr.next();
+
+            // gets all of the leaves at a depth
+            ArrayList<String> n = entry.getValue();
+
+            System.out.println(entry.getKey() + " | " + entry.getValue().size());
+        }
+    }
+
+    public static DFS buildGraph(DFS myGraph, Iterator<Entry> jsonItr) {
         while (jsonItr.hasNext()) {
             Entry e = jsonItr.next();
             List<List<String>> categories = e.getCategories();
 
-            // getting a title's list of every list of categories it is assigned to
+            // getting a product's list of every list of categories it is assigned to
             ListIterator<List<String>> outerItr = categories.listIterator();
 
             while (outerItr.hasNext()) {
                 // getting each sub-list
                 List<String> listofCats = outerItr.next();
-                // starts at 1 to avoid the root
-                ListIterator<String> innerItr = listofCats.listIterator(1);
+                ListIterator<String> innerItr = listofCats.listIterator(0);
 
+                // if not an empty trailing list (due to some kind of json parsing error)
                 if (!"".equals(listofCats.get(0))) {
-                    // check if edge already exists is done in addVertex
-                    // don't want to duplicate a category in list of outgoing neighbors
-                    if (!myGraph.hasPath("root", listofCats.get(0))) {
-                        myGraph.addVertex("root", listofCats.get(0));
-                        myGraph.addLeafDepth(0, listofCats.get(0));
-                    }
-                    // categories of length = 1
-                    if (!innerItr.hasNext()) {
-                        myGraph.addVertex(listofCats.get(0), e.getTitle());
-                        myGraph.addLeafDepth(1, e.getTitle());
-                    }
-                    int counter = 1;
-                    while (innerItr.hasNext()) {
-                        String c = innerItr.next();
 
-                        // checking if it is not a leaf node
-                        if (innerItr.hasNext()) {
-                            // don't want to duplicate a category in list of outgoing neighbors
-                            if (!myGraph.hasPath(c, listofCats.get(innerItr.nextIndex()))) {
-                                myGraph.addVertex(c, listofCats.get(innerItr.nextIndex()));
-                                counter++;
+                    // don't want to duplicate a category in list of outgoing neighbors
+                    if (!myGraph.hasPath("*", listofCats.get(0))) {
+                        // adding an edge from the root
+                        myGraph.addVertex("*", listofCats.get(0));
+                        myGraph.addLeafDepth(0, listofCats.get(0)); // store the depth of every leaf
+                    }
+                    if (listofCats.size() > 1) {
+                        int counter = 0; // stores level of tree
+
+                        // going through all of the categories
+                        while (innerItr.hasNext()) {
+                            String c = innerItr.next();
+                            // checking if it is not a leaf node
+                            if (innerItr.hasNext()) {
+                                // don't want to duplicate a category in list of outgoing neighbors
+                                if (!myGraph.hasPath(c, listofCats.get(innerItr.nextIndex()))) {
+                                    myGraph.addVertex(c, listofCats.get(innerItr.nextIndex()));
+
+                                    if (!myGraph.inLeafDepth(0, c)) {
+                                        myGraph.addLeafDepth(counter, c);
+                                    }
+                                }
                             }
-                        }
-                        // leaf of every entry is the title
-                        else {
-                            myGraph.addVertex(c, e.getTitle());
-                            myGraph.addVertex(e.getTitle(), null);
+                            // leaf of every entry is the title
+                            // the destination vertex of a leaf edge is null
+                            else {
+                                myGraph.addVertex(c, e.getTitle());
+                                if (!myGraph.inLeafDepth(counter, c)) {
+                                    myGraph.addLeafDepth(counter, c);
+                                }
+                                // error checking so that no leaf is repeated twice
+                                if (!myGraph.isVertex(e.getTitle())) {
+                                    myGraph.addVertex(e.getTitle(), null);
+                                    myGraph.addLeafDepth(counter + 1, e.getTitle());
+                                }
+                            }
                             counter++;
-                            myGraph.addLeafDepth(counter, e.getTitle());
+                        }
+                    } else {
+                        // add category
+                        myGraph.addVertex(listofCats.get(0), e.getTitle());
+
+                        // add leaf node (title)
+                        if (!myGraph.isVertex(e.getTitle())) {
+                            myGraph.addVertex(e.getTitle(), null);
+                            myGraph.addLeafDepth(1, e.getTitle());
                         }
                     }
                 }
             }
         }
+        return myGraph;
 
-        myGraph.printGraph();
+    }
 
+    public static void main(String[] args) throws IOException {
+
+        // read in .JSON dataset file
+        String dataPath = "/Users/jess/IGSProject/data/metadata_simplified.json";
+        BufferedReader reader = new BufferedReader(new FileReader(dataPath));
+        List<Entry> jsonEntries = new Gson().fromJson(reader, new TypeToken<List<Entry>>() {
+        }.getType());
+        reader.close();
+
+        // iterate over every JSON entry and add to graph
+        // each entry has two keys: title and categories
+        // title is the name of the product (the leaf)
+        // and categories is the path of product categories from the root to the leaf
+        Iterator<Entry> jsonItr = jsonEntries.iterator();
+
+        // default initialized with "*" as the root
+        DFS myGraph = buildGraph(new DFS(jsonEntries.size()), jsonItr);
+
+        // randomly selecting a target node
+        // String target = "Adult Ballet Tutu Cheetah Pink";
+        String target = getRandomTarget(myGraph);
+        System.out.println("The target is node " + target);
+        List<List<String>> path = new ArrayList<List<String>>();
+
+        Iterator<Entry> jsonItr2 = jsonEntries.iterator();
+        while (jsonItr2.hasNext()) {
+            Entry e = jsonItr2.next();
+            if (e.getTitle().equals(target)) {
+                path = e.getCategories();
+                break;
+            }
+        }
+        System.out.println("The path(s) to the target node are : ");
+        System.out.println(path);
+
+        // System.out.println(myGraph.getLeavesDepth());
+        // System.out.println(jsonEntries.size());
+        // getOutDegreeStats(myGraph.getLeavesDepth());
+
+        // System.out.println(myGraph.buildHeavyPathDFSTree());
+
+        // will need to call dfsinterleave for every node in order to get the node
+        // depth vs questions asked result
+
+        // List<String> listofNodes = new ArrayList<String>(graph.getNodes());
+        // for every node in list, set as target
+        // get depth of node
+        // do dfs interleave = returns number of questions
+        // store result as depth: total number of questions (summation)
+        // then for every depth, get the size of its leaves list, and divide summation
+        // by that size
     }
 
 }
--- DFS.java
+++ DFS.java
@@ -4,92 +4,162 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Stack;
 import java.util.Iterator;
+import java.util.List;
 import java.util.HashMap;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.ListIterator;
 
 public class DFS {
-    Map<String, ArrayList> map;
+    Map<String, ArrayList<String>> map;
     int size;
     String root;
-
+    Map<String, Boolean> visited;
     Map<Integer, ArrayList<String>> leavesByDepth;
 
     DFS(int size) {
         this.size = size;
-        this.root = "root";
-        map = new HashMap<String, ArrayList>();
+        this.root = "*";
+        map = new HashMap<String, ArrayList<String>>();
         leavesByDepth = new HashMap<Integer, ArrayList<String>>();
+
+        // initially seeting all vertices to visited = false
+        visited = new HashMap<String, Boolean>();
     }
 
-    void addVertex(String v, String newVertex) {
+    public void addVertex(String v, String newVertex) {
         if (!map.containsKey((v))) {
             ArrayList<String> adj = new ArrayList<String>();
 
+            // remember to do the "".equals if checking for a leaf
             if (newVertex != null) {
                 adj.add(newVertex);
             }
             map.put(v, adj);
-
+            visited.put(v, false);
         } else {
-            map.get(v).add(newVertex);
+            if (!isLeaf(v)) {
+                map.get(v).add(newVertex);
+            }
         }
     }
 
-    void addLeaf(int depth, String leaf) {
+    public boolean isVertex(String v) {
+        if (map.containsKey(v))
+            return true;
+        return false;
+    }
+
+    public boolean hasPath(String start, String dest) {
+
+        if (map.containsKey(start)) {
+            if (map.get(start).contains(dest)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addLeafDepth(int depth, String leaf) {
         if (!leavesByDepth.containsKey((depth))) {
             ArrayList<String> leaves = new ArrayList<String>();
             leaves.add(leaf);
             leavesByDepth.put(depth, leaves);
         }
-        leavesByDepth.get(depth).add(leaf);
+
+        if (!leavesByDepth.get(depth).contains(leaf)) {
+            leavesByDepth.get(depth).add(leaf);
+        }
     }
 
-    // TODO: bug here
-    void printGraph() {
-        Iterator<Map.Entry<String, ArrayList>> itr = map.entrySet().iterator();
+    public boolean inLeafDepth(int depth, String leaf) {
+        if (leavesByDepth.containsKey((depth)) && leavesByDepth.get(depth).contains(leaf)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void printGraph() {
+        Iterator<Map.Entry<String, ArrayList<String>>> itr = map.entrySet().iterator();
 
         while (itr.hasNext()) {
-            Map.Entry<String, ArrayList> entry = itr.next();
-            System.out.println(entry.getKey());
+            Map.Entry<String, ArrayList<String>> entry = itr.next();
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
+    }
+
+    public Map<Integer, ArrayList<String>> getLeavesDepth() {
+        return leavesByDepth;
+    }
+
+    public ArrayList<String> getNodes() {
+        return new ArrayList<String>(map.keySet());
+    }
+
+    boolean isLeaf(String vertex) {
+        // if list of out neighbors is empty
+        // System.out.println(map.get(vertex).isEmpty());
+        if (map.get(vertex).isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    // for DFS-interleave algorithm
+    public ArrayList<String> findLeaf(String vertex, Map<String, Boolean> visited) {
+        ArrayList<String> rootToLeafPath = new ArrayList<String>();
+
+        Stack<String> stack = new Stack<>();
+        stack.push(vertex);
+        rootToLeafPath.add(vertex);
+        System.out.println("pushing in findLeaf" + vertex);
+
+        while (stack.empty() == false) {
+            vertex = stack.pop();
+            visited.put(vertex, true);
+            rootToLeafPath.add(vertex);
+
+            if (isLeaf(vertex)) {
+                return rootToLeafPath;
+            } else {
+                if (!visited.get(vertex)) {
+                    stack.push(vertex);
+                }
+            }
+        }
+        return null;
+    }
+
+    public void dfsInterleave(ArrayList<String> ordering) {
+        System.out.println("hello world!");
     }
 
     // s = start vertex
-    public ArrayList<String> dfs_algo(String s) {
+    public ArrayList<String> buildHeavyPathDFSTree() {
 
+        // represents the traversal order of heavy-path DFS tree
         ArrayList<String> finalOrdering = new ArrayList<String>();
 
-        Map<String, Boolean> visited = new HashMap<String, Boolean>();
-        Iterator<Map.Entry<String, ArrayList>> visitedItr = map.entrySet().iterator();
-        while (visitedItr.hasNext()) {
-            Map.Entry<String, ArrayList> entry = visitedItr.next();
-            visited.put(entry.getKey(), false);
-        }
-
         Stack<String> stack = new Stack<>();
-        stack.push(s);
-        System.out.println("pushing " + s);
+        stack.push(this.root);
 
         while (stack.empty() == false) {
-            s = stack.pop();
+            String s = stack.pop();
             visited.put(s, true);
-            System.out.println("top of stack is " + s);
             finalOrdering.add(s);
-        }
 
-        // TODO: calculating the count of unvisited nodes for every out neighbor of node
-        // at top of stack
-        // Collections.sort(map.get(s), Collections.reverseOrder()); - this needs to be
-        // used for example in original paper
-        Iterator<Integer> countItr = map.get(s).iterator();
-        ArrayList<Integer> or = chooseMostOutreaching(countItr);
+            // Collections.sort(map.get(s), Collections.reverseOrder()); - used for first
+            // example in original paper instead of using heavy-path sort criteria
 
-        Iterator<Integer> colorItr = or.iterator();
-        while (colorItr.hasNext()) {
-            int v = colorItr.next();
-            if (!visited.get(v)) {
-                stack.push(v);
-                System.out.println("pushing " + v);
+            // if node at top of stack is not a leaf
+            if (!map.get(s).isEmpty()) {
+                ArrayList<String> outReachingOrder = chooseMostOutreaching(map.get(s).listIterator());
+                Iterator<String> oItr = outReachingOrder.listIterator();
+                while (oItr.hasNext()) {
+                    String v = oItr.next();
+                    stack.push(v); // don't need to test if visited bc that's already checked in
+                                   // chooseMostOutreaching
+                }
             }
         }
         return finalOrdering;
@@ -97,26 +167,65 @@ public class DFS {
 
     // iterating over all the out neighbors of a node to calculate number of white
     // edges
-    // put in error checking for length = 0 of out neighbors
-    // remember sorting algo will do the tie breaking for you
-    public ArrayList<Integer> chooseMostOutreaching(Iterator itr) {
-        // include the current node itself
-        int outreaching = 1;
+    // returns the list of children in order of count of white edges
+    public ArrayList<String> chooseMostOutreaching(ListIterator<String> itr) {
+        Map<String, Integer> nodeCount = new HashMap<String, Integer>();
+        ArrayList<String> outreaching = new ArrayList<String>();
+
         while (itr.hasNext()) {
-            int v2 = itr.next();
-            if (!visited.get(v2)) {
-                outreaching++;
+            String v = itr.next();
+            if (!visited.get(v)) {
+                int d = calculateDepth(v);
+                nodeCount.put(v, d);
             }
-            return outreaching;
         }
+        // sort the map based on depth
+        // sorting algorithm from
+        // https://stackoverflow.com/questions/12184378/sorting-linkedhashmap
+        List<Map.Entry<String, Integer>> entries = new ArrayList<Map.Entry<String, Integer>>(nodeCount.entrySet());
+        Collections.sort(entries, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> node1, Map.Entry<String, Integer> node2) {
+                return node1.getValue().compareTo(node2.getValue());
+            }
+        });
+        // append all neighbors in sorted order
+        for (Map.Entry<String, Integer> entry : entries) {
+            outreaching.add(entry.getKey());
+        }
+        return outreaching;
     }
 
-    public int recursiveTry(Iterator itr) {
-        if (Iterator.length == 0) {
-            return 1;
-        } else {
-            int total = chooseMostOutreaching(itr);
-            return 1 + total;
+    public int calculateDepth(String v) {
+        int depth = 0;
+        Iterator<String> itr = map.get(v).listIterator();
+        while (itr.hasNext()) {
+            String current = itr.next();
+            if (!visited.get(current)) {
+                Iterator<String> itr2 = map.get(current).listIterator();
+                while (itr2.hasNext()) {
+                    String current2 = itr2.next();
+                    if (!visited.get(current2))
+                        depth += map.get(current2).size();
+                }
+            }
         }
+        return depth;
     }
 }
+// doesn't work on large scale (stack overflow)
+/*
+ * public int calculateDepth(String v) {
+ * if (isLeaf(v)) {
+ * return 0;
+ * }
+ * int depthSum = 0;
+ * Iterator<String> itr = map.get(v).listIterator();
+ * while (itr.hasNext()) {
+ * String current = itr.next();
+ * if (!visited.get(current)) {
+ * depthSum += calculateDepth(current);
+ * }
+ * }
+ * return 1 + depthSum;
+ * }
+ */
